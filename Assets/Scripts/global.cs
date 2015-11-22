35919e7431c21d48443251b7fcf27daa9a39c989
Umut Seven <umutseven92@gmail.com>
2015-11-22 02:52:00 +0200

    Local play music mute, player name setting

--- Assets/Scripts/global.cs
+++ Assets/Scripts/global.cs
@@ -3,5 +3,11 @@ using System.Collections;
 
 public class global : MonoBehaviour
 {
+
     public static string GameVersion = "0.2.4";
+
+    // Player Pref values
+    public static string Music = "Music";
+    public static string SoundEffects = "SoundEffects";
+    public static string PlayerName = "PlayerName";
 }
--- Assets/Scripts/menuScript.cs
+++ Assets/Scripts/menuScript.cs
@@ -1,28 +1,44 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class menuScript : MonoBehaviour
 {
     public Text Version;
 
+    private readonly Random _rand = new Random();
+    private readonly string[] _names = { "Chuck", "Cedric", "Iggy", "Woodrow", "Fergie", "Martin", "Volpe", "Tommy", "Steve", "Townie", "Deuce", "Townie", "Marvin", "Nazo", "Clive", "Ula", "Hobo", "Punky", "Yuri", "Gus", "Stan", "Bill", "Salim", "Ed", "Rob", "Paul", "Ernosto", "Ramon" };
+
     // Use this for initialization
     void Start()
     {
-        if (!PlayerPrefs.HasKey("Music"))
-        {
-            PlayerPrefs.SetInt("Music", 1);
-        }
-
-        SetPrefs();
+        CheckPlayerPrefs();
+        SetMusic();
 
         Version.text = "v" + global.GameVersion;
     }
 
-    private void SetPrefs()
+    private void CheckPlayerPrefs()
+    {
+        if (!PlayerPrefs.HasKey(global.Music))
+        {
+            PlayerPrefs.SetInt(global.Music, 1);
+        }
+        if (!PlayerPrefs.HasKey(global.SoundEffects))
+        {
+            PlayerPrefs.SetInt(global.SoundEffects, 1);
+        }
+        if (!PlayerPrefs.HasKey(global.PlayerName))
+        {
+            PlayerPrefs.SetString(global.PlayerName, _names[_rand.Next(_names.Length)]);
+        }
+    }
+
+    private void SetMusic()
     {
         var audioSource = GameObject.Find("Speaker").GetComponent<AudioSource>();
 
-        audioSource.mute = PlayerPrefs.GetInt("Music") != 1;
+        audioSource.mute = PlayerPrefs.GetInt(global.Music) != 1;
     }
 }
