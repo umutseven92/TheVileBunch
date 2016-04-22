e616f0775b0df70a8a45b29d37b760c2853da597
Umut Seven <umutseven92@gmail.com>
2016-04-23 00:16:41 +0300

    Fix #30

--- Assets/Scripts/Online/graveyardOnlineScript.cs
+++ Assets/Scripts/Online/graveyardOnlineScript.cs
@@ -1,76 +1,107 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class graveyardOnlineScript : graveyardScript {
+public class graveyardOnlineScript : graveyardScript
+{
 
-	public Text OneOnline;
-	public Text[] TwoOnline;
-	public Text[] ThreeOnline;
+    public Text OneOnline;
+    public Text[] TwoOnline;
+    public Text[] ThreeOnline;
 
-	protected override void Start()
-	{
-		base.Start();
-		WinnerText.text = WinnerText.text.Replace("{class}", onlineSceneHelper.Winner);
-		var remaining = playerSelect.PlayerList;
+    public Button BtnPlayAgain;
 
-		remaining.Remove(remaining.Find(p=>p.Class.Equals(onlineSceneHelper.Winner)));
+    private bool playAgain;
 
-		switch (playerSelect.PlayerList.Count -1)
-		{
-			case 3:
-				Three[0].text= remaining[0].Class;
-				ThreeGrave[0].GetComponent<SpriteRenderer>().enabled = true;
-				ThreeCanvas[0].GetComponent<Canvas>().enabled = true;
+    protected override void Start()
+    {
+        base.Start();
+        WinnerText.text = WinnerText.text.Replace("{class}", onlineSceneHelper.Winner);
+        var remaining = playerSelect.PlayerList;
 
-				Three[1].text= remaining[1].Class;
-				ThreeGrave[1].GetComponent<SpriteRenderer>().enabled = true;
-				ThreeCanvas[1].GetComponent<Canvas>().enabled = true;
+        remaining.Remove(remaining.Find(p => p.Class.Equals(onlineSceneHelper.Winner)));
 
-				Three[2].text= remaining[2].Class;
-				ThreeGrave[2].GetComponent<SpriteRenderer>().enabled = true;
-				ThreeCanvas[2].GetComponent<Canvas>().enabled = true;
+        switch (playerSelect.PlayerList.Count - 1)
+        {
+            case 3:
+                Three[0].text = remaining[0].Class;
+                ThreeGrave[0].GetComponent<SpriteRenderer>().enabled = true;
+                ThreeCanvas[0].GetComponent<Canvas>().enabled = true;
 
-				ThreeOnline[0].text = remaining[0].OnlinePlayerName;
-				ThreeOnline[1].text = remaining[1].OnlinePlayerName;
-				ThreeOnline[2].text = remaining[2].OnlinePlayerName;
-				break;
-			case 2:
-				Two[0].text= remaining[0].Class;
-				TwoGrave[0].GetComponent<SpriteRenderer>().enabled = true;
-				TwoCanvas[0].GetComponent<Canvas>().enabled = true;
+                Three[1].text = remaining[1].Class;
+                ThreeGrave[1].GetComponent<SpriteRenderer>().enabled = true;
+                ThreeCanvas[1].GetComponent<Canvas>().enabled = true;
 
-				Two[1].text= remaining[1].Class;
-				TwoGrave[1].GetComponent<SpriteRenderer>().enabled = true;
-				TwoCanvas[1].GetComponent<Canvas>().enabled = true;
+                Three[2].text = remaining[2].Class;
+                ThreeGrave[2].GetComponent<SpriteRenderer>().enabled = true;
+                ThreeCanvas[2].GetComponent<Canvas>().enabled = true;
 
-				TwoOnline[0].text= remaining[0].OnlinePlayerName;
-				TwoOnline[1].text= remaining[1].OnlinePlayerName;
-				break;
-			case 1:
-				One.text= remaining[0].Class;
-				OneGrave.GetComponent<SpriteRenderer>().enabled = true;
-				OneCanvas.GetComponent<Canvas>().enabled = true;
+                ThreeOnline[0].text = remaining[0].OnlinePlayerName;
+                ThreeOnline[1].text = remaining[1].OnlinePlayerName;
+                ThreeOnline[2].text = remaining[2].OnlinePlayerName;
+                break;
+            case 2:
+                Two[0].text = remaining[0].Class;
+                TwoGrave[0].GetComponent<SpriteRenderer>().enabled = true;
+                TwoCanvas[0].GetComponent<Canvas>().enabled = true;
 
-				OneOnline.text = remaining[0].OnlinePlayerName;
-				break;
-		}
-	}
+                Two[1].text = remaining[1].Class;
+                TwoGrave[1].GetComponent<SpriteRenderer>().enabled = true;
+                TwoCanvas[1].GetComponent<Canvas>().enabled = true;
 
-	protected override void Update()
-	{
-		base.Update();
-		if (done)
-		{
-			GetBackToPlayerSelect();
-		}
-	}
+                TwoOnline[0].text = remaining[0].OnlinePlayerName;
+                TwoOnline[1].text = remaining[1].OnlinePlayerName;
+                break;
+            case 1:
+                One.text = remaining[0].Class;
+                OneGrave.GetComponent<SpriteRenderer>().enabled = true;
+                OneCanvas.GetComponent<Canvas>().enabled = true;
 
-	private static void GetBackToPlayerSelect()
-	{
-		playerSelect.PlayerList = new List<playerSelect.Player>();
-		PhotonNetwork.LoadLevel("OnlinePlayerSelect");
-	}
+                OneOnline.text = remaining[0].OnlinePlayerName;
+                break;
+        }
+
+        BtnPlayAgain.onClick.AddListener(PlayAgainButtonPressed);
+    }
+
+    private void PlayAgainButtonPressed()
+    {
+        BtnPlayAgain.enabled = false;
+
+        playAgain = true;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (done)
+        {
+            if (playAgain)
+            {
+                if (PhotonNetwork.isMasterClient)
+                {
+                    playerSelect.PlayerList = new List<playerSelect.Player>();
+                    PhotonNetwork.automaticallySyncScene = false;
+                    Time.timeScale = 1;
+                    PhotonNetwork.LoadLevel("OnlinePlayerSelect");
+                }
+            }
+            else
+            {
+                GetBackToMenu();
+            }
+        }
+    }
+
+    private static void GetBackToMenu()
+    {
+        playerSelect.PlayerList = new List<playerSelect.Player>();
+        PhotonNetwork.automaticallySyncScene = false;
+        Time.timeScale = 1;
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("Menu");
+    }
 
 }
